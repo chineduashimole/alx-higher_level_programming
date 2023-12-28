6e29f93f34d3996169ef5158d3079fd249903680
chineduashimole <cashimole@gmail.com>
2023-12-27 16:25:48 -0800

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    dList = set_1 & set_2
+    return (dList)
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    nw_list = set_1 ^ set_2
+    return (nw_list)

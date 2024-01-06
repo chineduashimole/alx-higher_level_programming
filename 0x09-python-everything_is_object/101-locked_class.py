7b02e54749eefedb29420742a39479705b827e0c
chineduashimole <cashimole@gmail.com>
2024-01-06 15:15:39 -0800

    Low memory cost 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,6 @@
 """This defines the term locked class"""
 
 class LockedClass:
-    """ Only allow initialization of the attribute first_name"""
+    """ Only allow instantiating of the attribute first_name"""
 
     __slots__ = ["first_name"]
